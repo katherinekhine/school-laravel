6bb08a4893769ee87100ea43c27fe13ed2f8852e
katherinekhine <khaingkatherine@gmail.com>
2024-07-09 10:40:40 +0630

    update index.blade.php of student

---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h1>All Students</h1>
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Action</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($students as $student)
+                    <tr>
+                        <td>{{$student->id}}</td>
+                        <td>{{$student->name}}</td>
+                        <td>{{$student->email}}</td>
+                        <td>
+                            <a href="{{route('students.show',['student' => $student]) }}" class="btn btn-link text-success">Show</a>
+                            <a href="{{route('students.edit',['student' => $student]) }}" class="btn btn-link">Edit</a>
+                            <form action="{{route('students.destroy', ['student' => $student])}}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete" class="btn btn-link text-danger">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
