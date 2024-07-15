1a03a55ac765477d8fa9137fa3c36ff12f328981
katherinekhine <khaingkatherine@gmail.com>
2024-07-15 18:32:54 +0630

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClassroomController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\TeacherController;
@@ -24,3 +25,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('teachers', TeacherController::class);
 Route::resource('subjects', SubjectController::class);
 Route::resource('students', StudentController::class);
+Route::resource('classrooms', ClassroomController::class);
